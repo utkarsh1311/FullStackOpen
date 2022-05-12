3382649f3bbaf0e4a35430fd05308e3fb71af378
Utkarsh <itsutkarsh123@gmail.com>
2022-05-12 22:36:49 +0530

    add solution for exercise 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
 
+const App = () => {
+	const anecdotes = [
+		"If it hurts, do it more often",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+	];
 
-function App() {
-  
-}
+	const [selected, setSelected] = useState(0);
+	const updateAnecdotes = () => {
+		let randomNumber = Math.floor(Math.random() * anecdotes.length);
+		console.log(randomNumber);
+		setSelected(randomNumber);
+	}
+
+	return (
+		<div>
+			<div>{anecdotes[selected]}</div>
+			<button onClick={updateAnecdotes}>
+				next anecdotes
+			</button>
+		</div>
+	);
+};
 
 export default App;
