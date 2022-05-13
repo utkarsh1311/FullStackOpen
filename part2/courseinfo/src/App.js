6731ce441cef34b2585d30f5902623415adc0c46
Utkarsh <itsutkarsh123@gmail.com>
2022-05-14 00:03:57 +0530

    add solution for exercise 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -10,40 +10,44 @@ const Part = ({ part }) => (
 
 const Content = ({ parts }) => (
 	<>
-		<Part part={parts[0]} />
-		<Part part={parts[1]} />
-		<Part part={parts[2]} />
+		{parts.map((part) => (
+			<Part key={part.id} part={part} />
+		))}
 	</>
 );
 
-const App = () => {
-	const course = "Half Stack application development";
-	const parts = [
-		{
-			name: "Fundamentals of React",
-			exercises: 10,
-		},
-		{
-			name: "Using props to pass data",
-			exercises: 7,
-		},
-		{
-			name: "State of a component",
-			exercises: 14,
-		},
-	];
-
+const Course = ({ course }) => {
 	return (
-		<div>
-			<Header course={course} />
-			<Content parts={parts} />
-			<Total
-				sum={
-					parts[0].exercises + parts[1].exercises + parts[2].exercises
-				}
-			/>
-		</div>
+		<>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+		</>
 	);
 };
 
+const App = () => {
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+				id: 3,
+			},
+		],
+	};
+
+	return <Course course={course} />;
+};
 export default App;
