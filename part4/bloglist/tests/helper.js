f6d98fa71fe5d7828f708a738cd69fcb86fe4dc8
Utkarsh <itsutkarsh123@gmail.com>
2022-09-29 16:51:48 +0530

    add test helper

---
+++ part4/bloglist/tests/helper.js
@@ -0,0 +1,42 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+	{
+		title: "React patterns",
+		author: "Michael Chan",
+		url: "https://reactpatterns.com/",
+		likes: 7,
+	},
+	{
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+		likes: 5,
+	},
+	{
+		title: "Canonical string reduction",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 12,
+	},
+];
+
+const nonExistingID = async () => {
+	const blog = new Blog({
+		title: "First class tests",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+		likes: 10,
+	});
+
+	await blog.save();
+	await blog.remove();
+	return blog._id.toString();
+};
+
+const blogsInDB = async () => {
+	const blogs = await Blog.find({});
+	return blogs.map(blog => blog.toJSON());
+};
+
+module.exports = { initialBlogs, nonExistingID, blogsInDB };
