abf05c4489813aee1e9fedb3bcd7c6c952d59740
Utkarsh <itsutkarsh123@gmail.com>
2022-09-29 16:06:00 +0530

    add solution to exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -24,18 +24,6 @@ const blogs = [
 		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
 		likes: 12,
 	},
-	{
-		title: "First class tests",
-		author: "Robert C. Martin",
-		url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-		likes: 10,
-	},
-	{
-		title: "TDD harms architecture",
-		author: "Robert C. Martin",
-		url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-		likes: 0,
-	},
 ];
 
 beforeEach(async () => {
@@ -43,13 +31,17 @@ beforeEach(async () => {
 	const blogObjects = blogs.map(blog => new Blog(blog));
 	const promiseArray = blogObjects.map(blog => blog.save());
 	await Promise.all(promiseArray);
-}, 100000);
+});
 
 test("get correct amount of blogs", async () => {
 	const response = await api.get("/api/blogs");
-	expect(response.body).toHaveLength(5);
-}, 100000);
+	expect(response.body).toHaveLength(3);
+});
 
+test("blog has property id", async () => {
+	const response = await Blog.find({});
+	expect(response[0]._id).toBeDefined();
+});
 afterAll(() => {
 	mongoose.connection.close();
 });
