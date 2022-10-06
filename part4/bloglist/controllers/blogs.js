99f3d843c313da7849f85ccadab914738295cf89
Utkarsh <itsutkarsh123@gmail.com>
2022-10-07 00:11:34 +0530

    add solution for exercise 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (req, res, next) => {
 	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -12,10 +13,23 @@ blogsRouter.get("/:id", async (req, res, next) => {
 	res.status(200).json(blog);
 });
 
+const getTokenFrom = req => {
+	const auth = req.get("authorization");
+	if (auth && auth.toLowerCase().startsWith("bearer")) {
+		return auth.substring(7);
+	}
+	return null;
+};
+
 blogsRouter.post("/", async (req, res) => {
 	const { title, author, url, likes, userId } = req.body;
+	const token = getTokenFrom(req);
+	const decodedToken = jwt.verify(token, process.env.SECRET);
 
-	const user = await User.findById(userId);
+	if (!decodedToken.id) {
+		return res.status(401).json({ error: "token missing or invalid " });
+	}
+	const user = await User.findById(decodedToken.id);
 
 	if (!title || !author || !url || !userId) {
 		return res.status(400).json({
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -19,6 +19,14 @@ const errorHandler = (error, req, res, next) => {
 		return res.status(400).send({ error: "Malformatted id" });
 	} else if (error.name === "ValidationError") {
 		return res.status(400).json({ error: error.message });
+	} else if (error.name === "JsonWebTokenError") {
+		return res.status(401).json({
+			error: "invalid token",
+		});
+	} else if (error.name === "TokenExpiredError") {
+		return res.status(401).json({
+			error: "token expired",
+		});
 	}
 	next(error);
 };
